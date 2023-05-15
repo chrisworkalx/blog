f31a7c9af2b8442484b127ee3c64f48e8a69852e
chrisworkalx <857717575@qq.com>
2023-05-15 18:25:18 +0800

    fix: 部署源文件

---
+++ documents/.vuepress/config.js
@@ -0,0 +1,23 @@
+const package = require('../../package.json');
+const { resolve } = require('path');
+module.exports = {
+  title: 'chrisworkalx的博客',
+  description: '用心记录每一天的生活',
+  base: `/${package.name}/`, // 以/${package.name}作为基准base
+  dest: './docs', //打包到指定的目录中
+  head: [['link', { rel: 'icon', href: `/superhuman.jpg` }]],
+  evergreen: true,
+  themeConfig: {
+    lastUpdated: '最后更新时间',
+    sidebar: require('./sidebar').sidebar,
+    nav: require('./sidebar').nav,
+    displayAllHeaders: true
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@image': resolve(__dirname, './public')
+      }
+    }
+  }
+};
